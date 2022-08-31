0067c23b57bd7890e86c7dc79502ea7d656c3f45
Mallory A. Ballinger <65674874+malballinger@users.noreply.github.com>
2022-08-31 10:20:38 -0600

    updated code

--- code/preprocess_popgen/SNPcallingGATK.sh
+++ code/preprocess_popgen/SNPcallingGATK.sh
@@ -9,9 +9,9 @@ samtools sort -o ${line}_merge.sort.bam ${line}_merge.bam
 
 picard MarkDuplicates INPUT=${line}_merge.sort.bam OUTPUT=${line}_markdups.bam METRICS_FILE=${line}_metrics.txt
 
-picard BuildBamIndex INPUT=${line}_markdups.bam
-
 picard AddOrReplaceReadGroups I=${line}_markdups.bam O=${line}_markdups.rehead.bam RGID=1 RGLB=lib1 RGPL=illumina RGPU=unit1 RGSM=${line}
+
+picard BuildBamIndex INPUT=${line}_markdups.rehead.bam
 
 gatk HaplotypeCaller -R Mus_musculus.GRCm38.dna.toplevel.fa -I ${line}_markdups.rehead.split.bam -ERC GVCF -stand-call-conf 20 -O ${line}_rawvariants.g.vcf.gz
 "
@@ -27,3 +27,4 @@ gatk --java-options \"-Xmx4g\" GenotypeGVCFs -R Mus_musculus.GRCm38.dna.toplevel
 gatk SelectVariants --reference Mus_musculus.GRCm38.dna.toplevel.fa --variant Combined_Ind.vcf.gz --select-type-to-include SNP --output Combined_Ind.SNPs.vcf.gz
 gatk VariantFiltration --reference Mus_musculus.GRCm38.dna.toplevel.fa --variant Combined_Ind.SNPs.vcf.gz --filter-expression \"QD < 2.0 || FS > 60.0\" --filter-name \"SNPFilter\" --output Combined_Ind.SNPsfilt.vcf.gz
 "
+done
